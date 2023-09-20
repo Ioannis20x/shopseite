8b6621524fb8d67e9597f92d335d27f0fccc481e
Ioannis <i.antoniadis@schommer-media.de>
2023-09-20 14:34:44 +0200

    main

--- import.php
+++ import.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once "db.php";
 
-// Datenbankverbindung herstellen
+// Datenbankverbindung 
 $servername = "localhost";
 $username = "root";
 $password = "";
